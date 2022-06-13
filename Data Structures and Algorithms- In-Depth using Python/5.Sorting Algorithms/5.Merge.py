156c3a19ff72587d316e6a97dbf444ff89aa7897
Prachurya Nath <prachuryanath00@gmail.com>
2022-06-13 20:33:29 +0530

    Create 5.Merge.py

---
+++ Data Structures and Algorithms- In-Depth using Python/5.Sorting Algorithms/5.Merge.py
@@ -0,0 +1,36 @@
+# Merge Sort O(nlogn)
+def mergesort(A, left, right):
+    if left < right:
+        mid=(left+right)//2
+        mergesort(A,left,mid)
+        mergesort(A,mid+1,right)
+        merge(A,left,mid,right)
+
+def merge(A,left,mid,right):
+    i = left
+    j = mid+1
+    k = left
+    B = [0]*(right+1)
+    while i <= mid and j <= right:
+        if A[i]<A[j]:
+            B[k]=A[i]
+            i=i+1
+        else:
+            B[k]=A[j]
+            j=j+1
+        k=k+1
+    while i<=mid:
+        B[k]=A[i]
+        i=i+1
+        k=k+1
+    while j<=right:
+        B[k]=A[j]
+        j=j+1
+        k=k+1
+    for x in range(left,right+1):
+        A[x]=B[x]
+
+A = [3,5,2,6,9,8]
+print('Original array:', A)
+mergesort(A,0,len(A)-1)
+print('Sorted array:',A)
